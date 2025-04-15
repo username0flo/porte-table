7630579b6a2c38db568a5b51ba28049e650589b2
username0flo <150714518+username0flo@users.noreply.github.com>
2025-04-15 13:07:53 +0200

    add colision functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from graphics import *
+from physics import *
 
 
 #input 
@@ -19,10 +20,32 @@ def setup_portal():
 def draw_game():
     pass
 
+def grid2window(i,j):
+    return (i * CELL_SIZE, j * CELL_SIZE)
+
+def load_tile(img_name):
+    charge_image(img_name)
+    modifie_taille_image(img_name,CELL_SIZE, CELL_SIZE)
+
+#init
+player = (rect(0,0,10,10), vector(0.0,0.0))
+
+WINDOW_W, WINDOW_H = 800, 600
+init_fenetre(WINDOW_W, WINDOW_H,"porte table")
+
+NB_CELL_W = WINDOW_W // 20
+NB_CELL_H = WINDOW_H // 20
+
+CELL_SIZE = WINDOW_W // NB_CELL_W # or : WINDOW_H // NB_CELL_H
+
+
+affiche_auto_off()
+# map: a dict of key = coordinates (i,j) and value = a bloc type
+map = {}
 
 #main
 
-while(1):
+while(pas_echap()):
     event = get_input()
     setup_portal()
     move_player()
--- physics.py
+++ physics.py
@@ -1,3 +1,5 @@
+import math
+
 # vector implementation: (x,y)
 
 GRAVITY = 9.8
@@ -8,9 +10,26 @@ def vector(x,y):
 def move_rect(rect, vect):
     x,y,w,h = rect
     vx,vy = vect
-    return (x + int(vx), y + int(vy), w, h)
+    return (x + vx, y + vy, w, h)
 
 def rect(x,y,w,h):
     return (x,y,w,h)
 
+def get_magnitude(vect):
+    x,y = vect
+    return math.sqrt(x**2 + y**2)
+
+def normalize(vect):
+    x,y = vect
+    magintude = get_magnitude(vect)
+    return (x / magintude, y / magintude)
+
+def point_in_rect(point, rect):
+    x,y = point
+    rx,ry,rw,rh = rect
+    return x >= rx and x <= rx+rw and y >= ry and y <= ry+rh
+
+def has_intersection(r1,r2):
+    x,y,w,h= r1
+    return point_in_rect((x,y),r2) or point_in_rect((x+w, y), r2) or point_in_rect((x, y + h),r2) or point_in_rect((x + w, y + h),r2)
 
